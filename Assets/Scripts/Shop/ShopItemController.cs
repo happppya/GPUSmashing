9a7bd53793d93cdb74907af728f5089b2908a86c
Happya <axlu810+github@gmail.com>
2026-07-12 20:00:49 -0500

    Added menu button functionality

---
+++ Assets/Scripts/Shop/ShopItemController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ShopItemController : MonoBehaviour
+{
+    [SerializeField] private GraphicsCardSO[] graphicsCards;
+    [SerializeField] private UpgradeSO[] upgrades;
+
+    [SerializeField] private Transform GPUContent;
+    [SerializeField] private Transform UpgradeContent;
+
+    [SerializeField] private GameObject GPUItemPrefab;
+    [SerializeField] private GameObject UpgradeItemPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Shop/ShopViewController.cs
+++ Assets/Scripts/Shop/ShopViewController.cs
@@ -1,12 +1,26 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
-public class ShopOpenController : MonoBehaviour
+public class ShopViewController : MonoBehaviour
 {
+    [Header("Shop Window Settings")]
     [SerializeField] private RectTransform shopWindow;
     [SerializeField] private float transitionTime = 0.2f;
     [SerializeField] private AnimationCurve easeCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+    [Header("Menu Button Dependencies")]
+    [SerializeField] private Button GPUButton;
+    [SerializeField] private Button UpgradeButton;
+    [SerializeField] private GameObject GPUScrollView;
+    [SerializeField] private GameObject UpgradeScrollView;
+    [SerializeField] private TMP_Text GPUText;
+    [SerializeField] private TMP_Text UpgradeText;
+
+    [Header("Menu Button Settings")]
+    [SerializeField] private Color defaultBackgroundColor;
 
     private Vector2 hiddenPosition;
     private Vector2 visiblePosition;
@@ -14,8 +28,39 @@ public class ShopOpenController : MonoBehaviour
     private enum ShopState { Closed, Opening, Open, Closing }
     private ShopState currentState = ShopState.Closed;
 
+    private void ToggleMenuButtons(Button selectedButton, TMP_Text selectedText, Button unselectedButton, TMP_Text unselectedText)
+    {
+        Image selectedButtonImage = selectedButton.GetComponent<Image>();
+        Image unselectedButtonImage = unselectedButton.GetComponent<Image>();
+
+        selectedButtonImage.color = Color.white;
+        unselectedButtonImage.color = defaultBackgroundColor;
+
+        selectedText.color = Color.black;
+        unselectedText.color = Color.white;
+    }
+
     private void Start()
     {
+        GPUScrollView.SetActive(false);
+        UpgradeScrollView.SetActive(false);
+
+        GPUButton.onClick.AddListener(() =>
+        {
+            GPUScrollView.SetActive(true);
+            UpgradeScrollView.SetActive(false);
+
+            ToggleMenuButtons(GPUButton, GPUText, UpgradeButton, UpgradeText);
+        });
+
+        UpgradeButton.onClick.AddListener(() =>
+        {
+            UpgradeScrollView.SetActive(true);
+            GPUScrollView.SetActive(false);
+
+            ToggleMenuButtons(UpgradeButton, UpgradeText, GPUButton, GPUText);
+        });
+
         visiblePosition = new Vector2(0f, shopWindow.anchoredPosition.y);
         hiddenPosition = new Vector2(shopWindow.rect.width, shopWindow.anchoredPosition.y);
 
